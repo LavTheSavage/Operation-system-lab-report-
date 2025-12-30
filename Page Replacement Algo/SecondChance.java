156d62341846e790cfbf85651de6fd5ab1590d0f
LavTheSavage <samyograi0ff@gmail.com>
2025-12-30 23:16:30 +0545

    Implement Second Chance page replacement algorithm

---
+++ Page Replacement Algo/SecondChance.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class SecondChance {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number of frames: ");
+            int frames = sc.nextInt();
+            System.out.print("Enter number of pages: ");
+            int n = sc.nextInt();
+            int[] pages = new int[n];
+            System.out.println("Enter page reference string:");
+            for (int i = 0; i < n; i++) pages[i] = sc.nextInt();
+            int[] mem = new int[frames];
+            boolean[] ref = new boolean[frames];
+            Arrays.fill(mem, -1);
+            int pointer = 0, faults = 0;
+            System.out.println("\nSecond Chance Page Replacement:");
+            for (int page : pages) {
+                boolean hit = false;
+                
+                for (int i = 0; i < frames; i++) {
+                    if (mem[i] == page) {
+                        ref[i] = true;
+                        hit = true;
+                        break;
+                    }
+                }
+                
+                if (!hit) {
+                    while (ref[pointer]) {
+                        ref[pointer] = false;
+                        pointer = (pointer + 1) % frames;
+                    }
+                    System.out.print("(Removed " + mem[pointer] + ") ");
+                    mem[pointer] = page;
+                    ref[pointer] = true;
+                    pointer = (pointer + 1) % frames;
+                    faults++;
+                }
+                System.out.print(page + " ");
+            }   System.out.println("\nTotal Page Faults: " + faults);
+        }
+    }
+}
